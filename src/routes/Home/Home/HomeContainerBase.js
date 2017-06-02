88acdb96d21670cd2ec9e0a0470eb8a9da4b37ad
Raul Leite <raul.abreu.leite@gmail.com>
2017-06-02 12:29:44 -0300

    yarn deploy:light

--- src/routes/Home/Home/HomeContainerBase.js
+++ src/routes/Home/Home/HomeContainerBase.js
@@ -21,33 +21,10 @@ const mapDispatchToProps = {
  * @return {Object} Propriedades relacionado ao Home
 */
 const mapStateToProps = ({home}) => {
-  console.log(
-    'home',
-    home.toJS()
-  )
-  console.log(
-    'home.loginHome',
-    home.getIn(['loginHome', 'user'])
-  )
-
-  // console.log('home.signup', home.signup.toJS())
-  // console.log('ownProps', ownProps)
   return {
-    // user: home.getIn(['loginHome', 'user']),
-    // styles: home.get('styles'),
-    // button: home.get('button'),
-    // errors: home.get('errors'),
-
     login: home.getIn(['home', 'login']),
     signup: home.getIn(['home', 'signup']),
-
-    // user: home.get('user'),
-    // styles: home.get('styles'),
-    // button: home.get('button'),
-    // errors: home.get('errors')
   }
-  // successMessage: home.get('successMessage'),
-  // captcha: home.get('captcha')
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)
